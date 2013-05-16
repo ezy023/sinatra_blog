c339c43a6e008757b12b9f57c79ad83fc88abdbd
ezy023 <allareri@gmail.com>
2013-05-16 10:42:07 -0500

    Add files to tracking

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,5 @@
+# post '/create_post' do
+# 	 @post = Post.new(:title => params[:title], :content => params[:content])
+#    @post.save
+# end
+	
---
+++ app/controllers/tag_controller.rb
@@ -0,0 +1,7 @@
+# post '/create_tag'
+#   @tag = Tag.new(:name = params[:name])
+#   # CREATE NEW TAG/POST RELATIONSHIP
+#   @tag.save
+
+# end
+
